4b2bcb5df9c4a8a30e9646acd439f869a936e330
李昱 <1121294648@qq.com>
2016-03-29 15:21:03 +0800

    单元测试

--- Sino.VipCardInfo/UnitTest/UnitTest1.cs
+++ Sino.VipCardInfo/UnitTest/UnitTest1.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sino.Web.VipCardInfo;
+using System.Threading.Tasks;
 
 namespace UnitTest
 {
-	[TestClass]
-	public class UnitTest1
-	{
-		[TestMethod]
-		public void TestMethod1()
-		{
-		}
-	}
+    [TestClass]
+    public class UnitTest1
+    {
+        private IVipCardInfo VipCardInfo;
+
+        [TestInitialize]
+        public void Init()
+        {
+            VipCardInfo = new ApixVipCardInfo();
+        }
+
+        [Description("银行卡信息查询")]
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var result = await VipCardInfo.QueryCardInfo("cd8afb38ec5c4a59507b7d5aa6713dfa", "6227001303530103472");
+            Assert.AreEqual(result.Code, 0);
+            Assert.AreEqual(result.Msg, "请求成功");
+            Assert.IsNotNull(result.Data);
+        }
+    }
 }
